33115dda24d52c59d4d4c127470b3918ba990642
AJ Granowski <AJGranowski@users.noreply.github.com>
2023-06-27 19:10:15 -0700

    Added success condition to CD

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const redditExpandedCommunityFilter = new RedditExpandedCommunityFilter();
 const storage = new Storage();
 
 debugMenu.draw();
-rrrrrrredditExpandedCommunityFilter.start()
+rrrrrrrredditExpandedCommunityFilter.start()
     .then(() => {
         if (storage.get(STORAGE_KEY.DEBUG)) {
             console.log("Stopped script.");
