093802181cd5f4ed94f3137551fdcd4bf0a1aecf
AJ Granowski <AJGranowski@users.noreply.github.com>
2023-06-27 19:00:13 -0700

    Reverted ci.yml

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const redditExpandedCommunityFilter = new RedditExpandedCommunityFilter();
 const storage = new Storage();
 
 debugMenu.draw();
-rrredditExpandedCommunityFilter.start()
+rrrredditExpandedCommunityFilter.start()
     .then(() => {
         if (storage.get(STORAGE_KEY.DEBUG)) {
             console.log("Stopped script.");
