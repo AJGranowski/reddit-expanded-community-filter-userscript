11f96fe4ecce3466dcfe1cfabe8a987d33da21a1
AJ Granowski <AJGranowski@users.noreply.github.com>
2023-06-27 19:07:08 -0700

    Removed lists

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const redditExpandedCommunityFilter = new RedditExpandedCommunityFilter();
 const storage = new Storage();
 
 debugMenu.draw();
-rrrrrredditExpandedCommunityFilter.start()
+rrrrrrredditExpandedCommunityFilter.start()
     .then(() => {
         if (storage.get(STORAGE_KEY.DEBUG)) {
             console.log("Stopped script.");
