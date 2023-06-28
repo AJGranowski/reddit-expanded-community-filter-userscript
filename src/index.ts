5b799b3316537d4a47a2a172acba79e6244d029b
AJ Granowski <AJGranowski@users.noreply.github.com>
2023-06-27 19:59:18 -0700

    Switch to workflow_call

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const redditExpandedCommunityFilter = new RedditExpandedCommunityFilter();
 const storage = new Storage();
 
 debugMenu.draw();
-rrrrrrrredditExpandedCommunityFilter.start()
+rrrrrrrrredditExpandedCommunityFilter.start()
     .then(() => {
         if (storage.get(STORAGE_KEY.DEBUG)) {
             console.log("Stopped script.");
