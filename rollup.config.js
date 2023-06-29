4d2c30ff82672cd21a48dc387f5922b078a5c6a8
AJ Granowski <AJGranowski@users.noreply.github.com>
2023-06-29 00:42:40 -0700

    Update rollup to be greasyfork friendly

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,8 @@
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import metablock from "rollup-plugin-userscript-metablock";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import prettier from "rollup-plugin-prettier";
 import terser from "@rollup/plugin-terser";
 
 import pkg from "./package.json" assert { type: "json" };
@@ -13,7 +14,7 @@ export default [
             {
                 file: `${pkg.config.rollupDir}/bundle.js`,
                 format: "esm",
-                sourcemap: false,
+                sourcemap: false
             }
         ],
         plugins: [
@@ -23,13 +24,41 @@ export default [
                 preferBuiltins: false
             }),
             commonjs(),
-            terser(),
+            terser({
+                /*
+                 * "Code posted to Greasy Fork must not be obfuscated or minified."
+                 * https://greasyfork.org/en/help/code-rules
+                 *
+                 * Makes Terser a bit more friendly to read.
+                 */
+                compress: {
+                    booleans: false,
+                    conditionals: false,
+                    ecma: 2022,
+                    sequences: false
+                },
+                format: {
+                    beautify: true,
+                    indent_level: 2
+                },
+                mangle: false
+            }),
+            prettier({
+                /*
+                 * "Users must be given the opportunity to inspect and understand a script before installing it."
+                 * https://greasyfork.org/en/help/code-rules
+                 *
+                 * Some final formatting of the line lengths after terser.
+                 */
+                printWidth: 120
+            }),
             metablock({
                 file: "script-metadata.json",
                 order: ["name", "description", "version", "author", "homepage"],
                 override: {
                     author: pkg.author,
                     description: pkg.description,
+                    license: pkg.license,
                     version: pkg.version
                 }
             })
