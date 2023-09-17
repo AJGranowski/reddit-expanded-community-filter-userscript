b295a9d9b2bb4bb7d28fe3eb626d24096ac79bd8
AJ Granowski <AJGranowski@users.noreply.github.com>
2023-09-16 21:52:30 -0700

    Add coverage thresholds

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,23 @@ export default {
         "html",
         "text"
     ],
+    coverageThreshold: {
+        global: {
+            branches: 50,
+            functions: 80,
+            statements: 80
+        },
+        "src/userscript/**": {
+            branches: 100,
+            functions: 100,
+            statements: 100
+        },
+        "src/utilities/**": {
+            branches: 100,
+            functions: 100,
+            statements: 100
+        }
+    },
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
     setupFilesAfterEnv: ["<rootDir>/tst/setup.ts"],
     testEnvironment: "jsdom",
