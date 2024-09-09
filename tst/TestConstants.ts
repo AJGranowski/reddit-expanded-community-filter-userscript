e3ee4fb5ff4d7a896b014f2759303f1788959bcc
AJ Granowski <AJGranowski@users.noreply.github.com>
2024-09-08 21:27:53 -0700

    Update SonarJS

--- tst/TestConstants.ts
+++ tst/TestConstants.ts
@@ -3,7 +3,7 @@ import path from "path";
 const HTML_ROOT = path.join(__dirname, "html");
 
 class TestConstants {
-    static HTML_PATH = {
+    static readonly HTML_PATH = {
         NEW_REDDIT: path.join(HTML_ROOT, "new.reddit.com.html"),
         SHREDDIT: path.join(HTML_ROOT, "sh.reddit.com.html")
     };
